e3e232502f181e799ee17f3267ff1c12f619f102
Olivier Dupuis <olivierdupuis@gmail.com>
2014-02-22 13:20:12 -0500

    New url

--- microFunctions.R
+++ microFunctions.R
@@ -2,4 +2,8 @@ library(ggplot2)
 
 qplot(data=dsIC, y=Price, x=Supply, main="Supply and Demand", geom="line")
 
-qplot(data=dsIC, aes(x=Supply, y=Price, shape=Line))
+ggplot(data=dsIC, aes(y=Price)) + 
+  geom_line(aes(x=Supply, color="Supply")) + 
+  geom_line(aes(x=Demand, color="Demand")) +
+  xlab("Quantity") + ylab("Price") + ggtitle("Supply and Demand")
+
